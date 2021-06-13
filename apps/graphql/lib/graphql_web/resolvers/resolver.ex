019b0ac78ca6603345125b0eadfb1c0206adff95
Natasha McIntyre <natashamc.cs@gmail.com>
2021-06-13 21:59:31 +0100

    questions

--- apps/graphql/lib/graphql_web/resolvers/resolver.ex
+++ apps/graphql/lib/graphql_web/resolvers/resolver.ex
@@ -1,4 +1,6 @@
 defmodule Graphql.Resolver do
+  # https://hexdocs.pm/absinthe/query-arguments.html
+  # These have 2 args, whereas docs have 3. Is it because they are namespaced Resolvers.Accounts... etc?
   def smoke_test(_args, _info) do
     {:ok, "Yes!"}
   end
